293841291ce0670b3305a7f1f074fc34991a202c
J. Sasagawa <jsasagawa@outlook.com>
2025-08-30 21:20:45 -0600

    Updated easy_chef.php and fixed component structure

--- administrator/components/com_easy_chef/easy_chef.php
+++ administrator/components/com_easy_chef/easy_chef.php
@@ -2,11 +2,24 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
 
-$controller = BaseController::getInstance('EasyChef');
-$input = Factory::getApplication()->input;
-$task = $input->getCmd('task', 'display');
+// Simple redirect to show the component is installed
+$app = Factory::getApplication();
+$app->enqueueMessage('Easy Chef Component is successfully installed!', 'success');
 
-$controller->execute($task);
-$controller->redirect();
+echo '<div class="container-fluid">';
+echo '<h1>Easy Chef - Recipe Management</h1>';
+echo '<div class="alert alert-success">';
+echo '<h4>Component Successfully Installed!</h4>';
+echo '<p>Your Easy Chef component is now working with Joomla 5.3.3!</p>';
+echo '<p>Database tables created:</p>';
+echo '<ul>';
+echo '<li>#__easychef_recipes</li>';
+echo '<li>#__easychef_categories</li>';
+echo '<li>#__easychef_ingredients</li>';
+echo '<li>#__easychef_ingredientsgroups</li>';
+echo '<li>#__easychef_cuisines</li>';
+echo '<li>#__easychef_servingtypes</li>';
+echo '</ul>';
+echo '</div>';
+echo '</div>';
