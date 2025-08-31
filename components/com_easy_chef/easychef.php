185452d4e5738e6ef163d935601edd5d42f4ef34
J. Sasagawa <jsasagawa@outlook.com>
2025-08-30 20:07:04 -0600

    Delete components/com_easy_chef directory

--- components/com_easy_chef/easychef.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
-$controller = BaseController::getInstance('EasyChef');
-$controller->execute(Factory::getApplication()->input->get('task'));
-$controller->redirect();
